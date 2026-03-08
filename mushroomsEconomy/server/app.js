b9171e4ba871f224406d30d56b4743e049bdad25
aleksandr813 <molokoflina@gmail.com>
2026-03-08 21:54:42 +0400

    modified:   mushroomsEconomy/server/app.js 	modified:   mushroomsEconomy/server/application/Answer.js 	deleted:    mushroomsEconomy/server/application/modules/exampleModule/Example.js 	deleted:    mushroomsEconomy/server/application/modules/exampleModule/ExampleManager.js 	modified:   mushroomsEconomy/server/application/modules/game/GameManager.js 	modified:   mushroomsEconomy/server/application/modules/mediator/Mediator.js

--- mushroomsEconomy/server/app.js
+++ mushroomsEconomy/server/app.js
@@ -1,22 +1,25 @@
+const CONFIG = require('./config');
+
 const express = require('express');
 const app = express();
 const server = require('http').createServer();
 const io = require('socket.io')(server, CONFIG.CORS);
 
-const CONFIG = require('./config');
 const Router = require('./application/router/Router');
 const DB = require('./application/modules/db/DB');
 const Mediator = require('./application/modules/mediator/Mediator');
 const Common = require('./application/modules/common/Common');
-const ExampleManager = require('./application/modules/exampleModule/ExampleManager');
+const Answer = require('./application/Answer');
+const GameManager = require('./application/modules/game/GameManager');
 
 const { NAME, PORT, DATABASE } = CONFIG;
 
 const db = new DB({ DATABASE });
 const mediator = new Mediator(CONFIG.MEDIATOR);
 const common = new Common();
+const answer = new Answer();
 
-new ExampleManager({ mediator, db, common, io });
+const gameManager = new GameManager( { mediator, db, common, io, answer } );
 
 app.use(express.static(`${__dirname}/public`));
 app.use('/', new Router(mediator));
