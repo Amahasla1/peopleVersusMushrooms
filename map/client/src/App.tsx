5940e700028281500affa8a27e52d1565d4132b2
G090l <ragogolev@mail.ru>
2026-03-18 21:07:11 +0400

    8008

--- map/client/src/App.tsx
+++ map/client/src/App.tsx
@@ -1,21 +1,22 @@
 import Store from './services/Store/Store';
 import Server from './services/server/Server';
 import PageManager from './pages/PageManager';
-
+import Mediator from './services/Mediator/Mediator';
 import './App.css';
 
 function App() {
   // mediator
-  const store = new Store();
-  const server = new Server();
+  const mediator = new Mediator({ EVENTS: {}, TRIGGERS: {} });
+  const store = new Store(mediator);
+  const server = new Server(mediator);
 
   server.check('ВАСИЛИЙ', 'Я на такое не подписывался!');
 
   return (
     <div className="App">
-        <div className='app'>
-          <PageManager />
-        </div>
+      <div className='app'>
+        <PageManager />
+      </div>
     </div>
   );
 }
