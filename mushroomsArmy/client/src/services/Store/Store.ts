dadbef2e9de0f06e0073062a498b3d801aba5ad4
LubutelbUgr <gleb.kha3412@gmail.com>
2026-03-21 20:42:31 +0300

    Update Store.ts

--- mushroomsArmy/client/src/services/Store/Store.ts
+++ mushroomsArmy/client/src/services/Store/Store.ts
@@ -2,26 +2,45 @@ import { TUser } from "../server/types";
 
 type TData = {
     token: string | null;
-    username: string | null;
-    [key: string]: any;
+    name: string | null;
+    [key: string]: unknown;
 }
 
 class Store {
     private data: TData = {
         token: null,
-        username: null
+        name: null
     }
 
-    set(name: string, value: any) {
+    set<K extends keyof TData>(name: K, value: TData[K]) {
         this.data[name] = value;
     }
 
-    get(name: string) {
+    get<K extends keyof TData>(name: K): TData[K] {
         return this.data[name];
     }
 
-    clear(name: string) {
-        this.data[name] = null;
+    clear<K extends keyof TData>(name: K) {
+        if (name in this.data) {
+            this.data[name] = null;
+        }
+    }
+
+    setUser(user: TUser) {
+        this.data.token = user.token;
+        this.data.name = user.name;
+    }
+
+    getUser(): { token: string | null; name: string | null } {
+        return {
+            token: this.data.token,
+            name: this.data.name
+        };
+    }
+
+    clearUser() {
+        this.data.token = null;
+        this.data.name = null;
     }
 }
 
