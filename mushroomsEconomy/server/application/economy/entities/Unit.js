b8010410cd730dbeeded33b9c07abef377bdad5d
golpgelop <golpgelop@gmail.com>
2026-03-27 00:33:55 +0400

    modified:   mushroomsEconomy/server/application/economy/entities/Unit.js

--- mushroomsEconomy/server/application/economy/entities/Unit.js
+++ mushroomsEconomy/server/application/economy/entities/Unit.js
@@ -1,16 +1,13 @@
-import CONFIG from "../../../config";
-
 class Unit {
     constructor({ x, y, guid, map, easystar }) {
         this.x = x;
         this.y = y;
         this.guid = guid;
         this.easystar = easystar;
-        this.map = map;
-        //Переменные которые обновятся в дочерних классах
+        this.map = map;           // динамическая карта (обновляется извне)
         this.hp = 1;
         this.speed = 1;
-
+        this.isMoving = false;
     }
 
     get() {
@@ -19,7 +16,7 @@ class Unit {
             y: this.y,
             hp: this.hp,
             guid: this.guid
-        }
+        };
     }
 
     findPath(targetX, targetY) {
@@ -29,36 +26,44 @@ class Unit {
         return new Promise((resolve) => {
             const attemptFindPath = () => {
                 attempts++;
-
+                if (this.easystar.setGrid) this.easystar.setGrid(this.map);
                 this.easystar.findPath(this.x, this.y, targetX, targetY, (path) => {
                     if (path) {
                         resolve(path);
                     } else if (attempts < maxAttempts) {
-                        setTimeout(() => {
-                            attemptFindPath();
-                        }, 1000);
+                        setTimeout(() => attemptFindPath(), 1000);
                     } else {
                         resolve(null);
                     }
                 });
                 this.easystar.calculate();
             };
-
             attemptFindPath();
         });
     }
 
     async moveTo(targetX, targetY) {
-        const path = await this.findPath(targetX, targetY);
+        if (this.isMoving) return false;
+        this.isMoving = true;
 
-        if (!path) return false;
+        const stepDelay = 100 / this.speed;
 
-        for (const point of path.slice(1)) {
-            this.x = point.x;
-            this.y = point.y;
-            await new Promise(resolve => setTimeout(resolve, 100));
+        while (true) {
+            if (this.x === targetX && this.y === targetY) break;
+
+            const path = await this.findPath(targetX, targetY);
+            if (!path || path.length < 2) break;
+
+            const nextStep = path[1];
+            if (this.map[nextStep.y][nextStep.x] !== 0) break;
+
+            this.x = nextStep.x;
+            this.y = nextStep.y;
+
+            await new Promise(resolve => setTimeout(resolve, stepDelay));
         }
 
+        this.isMoving = false;
         return true;
     }
 }
